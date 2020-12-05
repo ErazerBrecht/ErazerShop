888500d26ce39c74f40a3780e743beb005031851
Brecht Carlier <erazerbrecht@gmail.com>
2020-12-05 17:26:17 +0100

    initial setup erazershop.idsrv setup identityserver with identity / ef core npgsql

---
+++ ErazerShop.Idsrv/Config.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using Duende.IdentityServer.Models;
+using System.Collections.Generic;
+
+namespace ErazerShop.Idsrv
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email() 
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("erazershop.api"),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource("erazershop.api")
+                {
+                    Scopes = {"erazershop.api"}
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "erazershop",
+                    ClientSecrets = {new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256())},
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireConsent = false,
+
+                    RedirectUris = {"https://localhost:9999/signin-oidc"},
+
+                    AllowOfflineAccess = false,
+                    AllowedScopes = {"openid", "profile", "email", "erazershop.api"},
+                    
+                    AccessTokenLifetime = 1800
+                },
+            };
+    }
+}
